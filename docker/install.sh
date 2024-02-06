b4d8e2b47ffa575fb53aa1b02a8f65ba397226f3
Tyron Gabriel <51530686+tyrongabriel@users.noreply.github.com>
2024-02-06 21:57:00 +0100

    Updated docker script

--- docker/install.sh
+++ docker/install.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
+
+# Run script as sudo with sudo bash ./install.sh
 # Add Docker's official GPG key:
-apt-get update
-apt-get install ca-certificates curl gnupg
+apt update
+apt install ca-certificates curl gnupg
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 chmod a+r /etc/apt/keyrings/docker.gpg
@@ -10,10 +13,10 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get update
+apt update
 
 # Install latest version
-apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Test installation
-sudo docker run hello-world
+docker run hello-world
